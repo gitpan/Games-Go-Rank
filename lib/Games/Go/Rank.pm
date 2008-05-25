48d8b2337d9a2f3381fd184410db44143b03b71e
Marcel Grünauer <marcel@cpan.org>
2008-05-25 17:14:48 +0000

    Import of MARCEL/Games-Go-Rank-0.05 from CPAN.
    gitpan-cpan-distribution: Games-Go-Rank
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         MARCEL/Games-Go-Rank-0.05.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Games/Go/Rank.pm
+++ lib/Games/Go/Rank.pm
@@ -2,22 +2,15 @@ package Games::Go::Rank;
 
 use warnings;
 use strict;
+use Moose;
 
-
-our $VERSION = '0.04';
-
-
-use base 'Class::Accessor::Complex';
-
+our $VERSION = '0.05';
 
 use overload
     '""'  => 'stringify',
     '<=>' => 'num_cmp';
 
-
-__PACKAGE__
-    ->mk_new
-    ->mk_accessors(qw(rank));
+has 'rank' => (is => 'rw');
 
 
 sub stringify {
@@ -98,46 +91,11 @@ on. You can also use other common formats such as C<6-dan> or C<2 dan>.
 Anything after the first C<k>, C<d> or C<p> is ignored. So C<6-dan*> is the
 same as C<6-dan>, which is the same as C<6d>.
 
-Games::Go::Rank inherits from L<Class::Accessor::Complex>.
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    carp(), cluck(), croak(), flatten(), mk_abstract_accessors(),
-    mk_array_accessors(), mk_boolean_accessors(),
-    mk_class_array_accessors(), mk_class_hash_accessors(),
-    mk_class_scalar_accessors(), mk_concat_accessors(),
-    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
-    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
-    mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor(), subname()
-
 =head1 METHODS
 
 =over 4
 
-=item new
 
-    my $obj = Games::Go::Rank->new;
-    my $obj = Games::Go::Rank->new(%args);
-
-Creates and returns a new object. The constructor will accept as arguments a
-list of pairs, from component name to initial value. For each pair, the named
-component is initialized by calling the method of the same name with the given
-value. If called with a single hash reference, it is dereferenced and its
-key/value pairs are set as described before.
 
 =item as_value
 
@@ -154,6 +112,12 @@ Sets the rank from a numerical value that is interpreted as described above.
 
 =back
 
+Games::Go::Rank inherits from L<Moose::Object>.
+
+The superclass L<Moose::Object> defines these methods and functions:
+
+    new(), BUILDALL(), DEMOLISHALL(), DESTROY(), does(), dump()
+
 =head1 TAGS
 
 If you talk about this module in blogs, on del.icio.us or anywhere else,
@@ -161,7 +125,7 @@ please use the C<gamesgorank> tag.
 
 =head1 VERSION 
                    
-This document describes version 0.04 of L<Games::Go::Rank>.
+This document describes version 0.05 of L<Games::Go::Rank>.
 
 =head1 BUGS AND LIMITATIONS
 
@@ -181,13 +145,13 @@ The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
 site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+Copyright 2007-2008 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
